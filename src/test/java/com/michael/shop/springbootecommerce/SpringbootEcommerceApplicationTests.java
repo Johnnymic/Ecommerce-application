e45c4a025584c948883ef508a34a7d63a1aeba09
John Michael <johnny25580@gmail.com>
2023-04-28 16:44:07 +0100

    first commit

---
+++ src/test/java/com/michael/shop/springbootecommerce/SpringbootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.shop.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
